ef4090eaff2da54ffe5d94e024eb088d792698f9
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-06-24 11:17:06 -0700

    complete designing mulit-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,29 @@
+DROP DATABASE chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
+
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  username varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id int NOT NULL AUTO_INCREMENT,
+  users_id int NOT NULL,
+  content varchar(255) NOT NULL,
+  roomname varchar(255),
+  PRIMARY KEY (id),
+  FOREIGN KEY (users_id) REFERENCES users(id)
 );
 
 /* Create other tables and define schemas for them here! */
 
 
 
-
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
